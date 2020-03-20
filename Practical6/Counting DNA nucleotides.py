b0cea14eebaac274eb769fd1cf13b45f880d4173
WZJin <Zijin.19@intl.zju.edu.cn>
2020-03-20 11:53:21 +0800

    Practical6 assignment1

---
+++ Practical6/Counting DNA nucleotides.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 09:55:24 2020
+
+@author: shen
+"""
+temp = input('Please input a gene sequence:')
+gene = temp
+a = gene.count('A')
+b = gene.count('T')
+c = gene.count('G')
+d = gene.count('C')
+
+e = a + b + c + d
+a1 = str(a*100/e)[:4] + '%'
+b1 = str(b*100/e)[:4] + '%'
+c1 = str(c*100/e)[:4] + '%'
+d1 = str(d*100/e)[:4] + '%'
+genedic1 = {'A':str(a1), 'T':str(b1), 'G':str(c1), 'C':str(d1)}
+print(genedic1)  
+
+import matplotlib.pyplot as plt
+labels = 'A', 'T', 'G', 'C'
+sizes = [a, b, c, d]
+plt.pie(sizes, labels = labels, autopct = '%1.1f%%', startangle = 50)
+plt.axis('equal')
+plt.show
