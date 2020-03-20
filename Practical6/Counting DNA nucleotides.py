ccfb561fa8290ffb7a9ec08e9ee62e042d1181d7
WZJin <Zijin.19@intl.zju.edu.cn>
2020-03-20 11:56:30 +0800

    Add the pseudocode to the code.

--- Practical6/Counting DNA nucleotides.py
+++ Practical6/Counting DNA nucleotides.py
@@ -9,19 +9,19 @@ gene = temp
 a = gene.count('A')
 b = gene.count('T')
 c = gene.count('G')
-d = gene.count('C')
+d = gene.count('C')# Count the numbers of every basic groups.
 
 e = a + b + c + d
 a1 = str(a*100/e)[:4] + '%'
 b1 = str(b*100/e)[:4] + '%'
 c1 = str(c*100/e)[:4] + '%'
-d1 = str(d*100/e)[:4] + '%'
+d1 = str(d*100/e)[:4] + '%'# Convert all the numbers into percentages.
 genedic1 = {'A':str(a1), 'T':str(b1), 'G':str(c1), 'C':str(d1)}
-print(genedic1)  
+print(genedic1)  #Build a dictionary.
 
 import matplotlib.pyplot as plt
 labels = 'A', 'T', 'G', 'C'
 sizes = [a, b, c, d]
 plt.pie(sizes, labels = labels, autopct = '%1.1f%%', startangle = 50)
 plt.axis('equal')
-plt.show
+plt.show # Draw a pie.
